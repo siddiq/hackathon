72cdaaf8d710f06cadf6350d72b8c98f15201a09
Siddiq Akbar <siddiq@one.com>
2015-08-01 12:04:58 +0400

    Show picture in a <img>

--- www/main/main.controller.js
+++ www/main/main.controller.js
@@ -1,37 +1,47 @@
-/*global ionic, angular */
+/*global ionic, angular, Camera, ProductsController */
 
 function MainController ($scope) {
     this.$scope = $scope;
     $scope.vm = this;
 
-//    ionic.Platform.ready(function() {
+    function onDeviceReady() {
+        $scope.vm.initcamera();
+    }
 
-        function onDeviceReady() {
-            $scope.vm.initcamera();
-        }
-
-        if (navigator.camera) {
-            console.log('Plugin found');
-            onDeviceReady();
-        } else {
-            console.log('Plugin not found, wait for device ready');
-            document.addEventListener("deviceready", onDeviceReady, false);
-        }
-//    });
+    if (navigator.camera) {
+        console.log('Plugin found');
+        onDeviceReady();
+    } else {
+        console.log('Plugin not found, wait for device ready');
+        document.addEventListener('deviceready', onDeviceReady, false);
+    }
 }
 
 angular.extend(MainController.prototype, {
     initcamera: function () {
         if (navigator.camera) {
-            
-            navigator.camera.getPicture(function(imageURI) {
-                // imageURI is the URL of the image that we can use for
-                // an <img> element or backgroundImage.
-                alert(imageURI);
-            }, function(err) {
+
+            navigator.camera.getPicture(function (imageURI) {
+                document.getElementById('myimage').src = 'data:image/jpg;base64,' + imageURI;
+            }, function (err) {
                 // Ruh-roh, something bad happened
-            }, {});
-            
+                navigator.notification.alert(err, 'Error');
+            }, {
+                quality: 75,
+                destinationType: Camera.DestinationType.DATA_URL,
+                sourceType: Camera.PictureSourceType.CAMERA,
+
+                //allowEdit: true,     // its not working in iphone anyway
+                encodingType: Camera.EncodingType.JPEG,     // we probably dont need this but just in case
+
+                // Make the picture a bit smaller and easy to send over to API.
+                targetWidth: 600,
+                targetHeight: 600,
+
+                //popoverOptions: CameraPopoverOptions,
+                saveToPhotoAlbum: false
+            });
+
             this.$scope;
         } else {
             // desktop
